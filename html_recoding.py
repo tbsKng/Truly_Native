15025cb3d6b81a180ed9bb21eb1050616fff8fce
tbsKng <tobias.koenig89@gmx.de>
2015-09-01 22:29:59 +0200

    Now with Iteration counting

--- html_recoding.py
+++ html_recoding.py
@@ -1,12 +1,11 @@
 import os 
 import glob
 
-Path=raw_input("Define Path: ")
-
+Path="/home/tobi/Schreibtisch/Kaggle/Data/1"
+count=0
 for filename in glob.glob(os.path.join(Path, '*.txt')):
 	f=filename
-	#f=open(filename,'r')
-	#print f.read()
+	count +=1
+	print "iteration:" count 
 	name=os.path.splitext(f)[0]
 	os.rename(f,name+".html")
-	#f.close()
